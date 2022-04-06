679f39e08b908392fa36cfb6f45e9b451ddb5566
sill214 <ppink214@gmail.com>
2022-04-06 15:08:30 +0900

    Quiz 추가

--- function.js
+++ function.js
@@ -145,3 +145,27 @@ const add = (a, b) => a + b;
 (function hello() {
     console.log('IIFE');
 })();
+
+
+// Fun Quiz time
+// function calculate(command, a, b)
+// command: add, substract, divide, multiply, remainder
+
+function calculate(command, a, b) {
+    switch(command) {
+        case 'add':
+            return a + b;
+        case 'substract':
+            return a - b;
+        case 'divide':
+            return a / b;
+        case 'multiple':
+            return a * b;
+        case 'remainder':
+            return a % b;
+        default:
+            throw Error('unknown command');
+    }
+}
+
+console.log(calculate('add', 2, 3));
