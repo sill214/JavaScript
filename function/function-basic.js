524e3b67f1953639502067f974ff4c881a00e847
sill214 <ppink214@gmail.com>
2022-04-15 15:32:21 +0900

    JavaScript Function Basic

---
+++ function/function-basic.js
@@ -0,0 +1,17 @@
+// Don't give up!
+
+// 함수 선언
+function doSomething() {
+    console.log('hello');
+}
+
+function add(a, b) {
+    const sum = a + b;
+    return sum;
+}
+
+// 함수 호출
+doSomething();
+
+const result = add(1, 2);
+console.log(result);
