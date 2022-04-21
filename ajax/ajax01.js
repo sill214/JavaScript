2cba89ace544e5e4b0a2626dac8ddbaf3997403f
sill214 <ppink214@gmail.com>
2022-04-21 16:42:04 +0900

    생 활 코 딩 AJAX

---
+++ ajax/ajax01.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// 1. Old 
+// const ajax = new XMLHttpRequest();
+// ajax.onreadystatechange = function() {
+//     if(this.readyState == 4 && this.status == 200) {
+//         console.log(ajax.responseText);
+//     }
+// };
+// ajax.open("GET", "https://codingapple1.github.io/price.json", true);
+// ajax.send();
+
+// 2. fetch
+// fetch('https://codingapple1.github.io/price.json')
+//     .then((response) => {
+//         if(!response.ok) {
+//             throw new Error('400 or 500 Error!')
+//         }
+//         return response.json(); // 수신한 데이터
+//     })
+//     .then((result) => {
+//         console.log(result);
+//     })
+//     .catch(() => {
+//         console.log('Error!');
+//     });
+
+// Async or await
+// async function getData() {
+//     const response = await fetch('https://codingapple1.github.io/price.json');
+//     if(!response.ok) {
+//         throw new Error('400 or 500 Error!');
+//     }
+//     const result = await response.json();
+//     console.log(result);
+// }
+// getData().catch(() => {
+//     console.log('Error!')
+// });
+
+// 3. 외부 라이브러리 방식
+// Axios
+axios.get('https://codingapple1.github.io/price.json')
+    .then((result) => {
+        console.log(result.data);
+    })
+    .catch(() => {
+        console.log('Error!');
+    })
